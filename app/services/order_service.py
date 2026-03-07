7705dc4d918e3756bc8de7566e8bc9a16aa823dc
Alperen Torun <alperentorun334@gmail.com>
2026-03-08 01:34:49 +0300

    Feature: Request and display Customer Instagram Username on Orders

--- app/services/order_service.py
+++ app/services/order_service.py
@@ -18,6 +18,7 @@ class OrderService:
         source: OrderSource = OrderSource.WEB,
         payment_method: PaymentMethod = PaymentMethod.CREDIT_CARD,
         email: str = None,
+        social_username: str = None,
         shopify_invoice_url: str = None
     ) -> Order:
         new_order = Order(
@@ -30,6 +31,7 @@ class OrderService:
             source=source,
             payment_method=payment_method,
             email=email,
+            social_username=social_username,
             shopify_invoice_url=shopify_invoice_url,
             status=OrderStatus.PENDING
         )
